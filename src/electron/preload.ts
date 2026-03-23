112ab86724d7c551ef4e3a82e45df9c914ef5a1f
nirbhay-vora <nirbhayvora9615@gmail.com>
2026-03-23 15:19:12 +0530

    fix(preload): expose postureUpdate to renderer

--- src/electron/preload.ts
+++ src/electron/preload.ts
@@ -4,6 +4,15 @@ contextBridge.exposeInMainWorld('electronAPI', {
   notify: (title: string, body: string) => {
     ipcRenderer.send('show-notification', { title, body })
   },
+  postureUpdate: (data: {
+    percent: number
+    deviationThreshold: number
+    slouchSeconds: number
+    cooldownSeconds: number
+    feedback?: string
+  }) => {
+    ipcRenderer.send('posture-update', data)
+  },
   dimScreen: () => {
     ipcRenderer.send('dim-screen')
   },
